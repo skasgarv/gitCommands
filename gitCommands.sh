61a1bcd27a598cfc1c1c90291e27a2c027a9691b
Suhas Murthy <suhas.murthy@bnymellon.com>
2017-02-08 15:04:12 -0500

    skasgarv-01: Files staged and commit message formatted

--- gitCommands.sh
+++ gitCommands.sh
@@ -10,3 +10,16 @@ ticketNumber=$(echo $branchName |cut -d "-" -f2);
 gitTag="skasgarv-" #modify to suit the tag name 
 #concatenate ticket number and tag name
 tagName="$gitTag$ticketNumber" 
+
+#Stage files
+echo "Adding files..."
+git add -A
+echo "Files staged"
+echo "=================================================================================="
+
+#Read commit message from user
+echo "Enter Commit message:" 
+read textValue
+
+#Generate commit message
+commitMsg="$tagName:$textValue"
